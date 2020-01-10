a61f8087fb845a8bd7868d3008937f6f97a13899
svenvoskamp <sven.voskamp@student.howest.be>
2020-01-10 16:48:14 +0100

    Finished spotlight section #3

--- src/view/products/index.php
+++ src/view/products/index.php
@@ -1,9 +1,14 @@
 <section class = "section__spotlight">
-  <div class = "container section__spotlight--main">
+  <div class = "container__dashed section__spotlight--main">
     <h2 class = "h2__spotlight"><span class = "h2__background--red">In de spotlight</span></h2>
       <div class = "spotlight__textcontent">
-        <p class = "p__spotlight--header"></p>
-        <p class = "p__spotlight--header"></p>
+        <p class = "p__spotlight--header">Boekenactie 2020 - Week 6:</p>
+        <p class = "p__spotlight--title">The Road - Cormac Mccarthy</p>
+      </div>
+      <div class="spotlight__explained">
+        <p class = "p__spotlight--explained">*Bij aankoop van een HUMO magazine van deze week ontvangt u een kortingscode die is te
+          gebruiken op de 10 actieboeken. </p>
+        <button class = "button__spotlight">&rarr; Ontdek "The Road"</button>
       </div>
   </div>
 </section>
