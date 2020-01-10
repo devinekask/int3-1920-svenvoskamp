1380c081f7976184b7ad604370cf96826fa0e06a
svenvoskamp <sven.voskamp@student.howest.be>
2020-01-10 15:21:12 +0100

    Navbar finished #2

--- src/view/layout.php
+++ src/view/layout.php
@@ -8,6 +8,35 @@
   </head>
   <body>
     <header>
+      <nav>
+        <div class = "nav__top container">
+          <div class = "nav__top--left">
+            <li class = "li__nav--left"><span class = span__nav--background>Video</span></li>
+            <li class = "li__nav--left">TV-Gids</li>
+            <li class = "li__nav--left">Zoekertjes</li>
+            <li class = "li__nav--left">Abonnement Huren</li>
+          </div>
+          <div class = "nav__top--right">
+            <li class = "li__nav--right"><span class = span__nav--red>Nu in HUMO</span></li>
+            <li class = "li__nav--right">Login</li>
+            <li class = "li__nav--right">Registreer</li>
+            <li class = "li__nav--right">Winkelmand (<span class = span__nav--red>1</span>)</li>
+          </div>
+        </div>
+        <div class="nav__main">
+          <div class = "container nav__main--flex">
+            <li class = "li__nav--main">Home</li>
+            <li class = "li__nav--main">Actua</li>
+            <li class = "li__nav--main">Humor</li>
+            <li class = "li__nav--main">TV/Film</li>
+            <img class = "img__nav--main" src="../assets/img/logo.png" alt="Humo logo">
+            <li class = "li__nav--main">Muziek</li>
+            <li class = "li__nav--main">Boeken</li>
+            <li class = "li__nav--main"><span class = "span__main--background">Shop</span></li>
+            <img width = "24" height = "24" class = "img__nav--main img__nav--search" src="../assets/img/search.png" alt="Search Icon">
+          </div>
+        </div>
+      </nav>
       </header>
     <main>
       <?php echo $content;?>
---
+++ src/view/products/index.php
@@ -0,0 +1,9 @@
+<section class = "section__spotlight">
+  <div class = "container section__spotlight--main">
+    <h2 class = "h2__spotlight"><span class = "h2__background--red">In de spotlight</span></h2>
+      <div class = "spotlight__textcontent">
+        <p class = "p__spotlight--header"></p>
+        <p class = "p__spotlight--header"></p>
+      </div>
+  </div>
+</section>
